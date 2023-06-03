a5463eece3490429004a41d4ef151984d1f05ba2
frank89722 <mail@frankv.me>
2023-06-03 20:56:38 +0800

    implement config for modloader

--- Fabric/src/main/java/me/frankv/staaaaaaaaaaaack/Staaaaaaaaaaaack.java
+++ Fabric/src/main/java/me/frankv/staaaaaaaaaaaack/Staaaaaaaaaaaack.java
@@ -1,12 +1,27 @@
 package me.frankv.staaaaaaaaaaaack;
 
+import lombok.extern.slf4j.Slf4j;
+import me.frankv.staaaaaaaaaaaack.config.FiberUtils;
+import me.frankv.staaaaaaaaaaaack.config.StxckFiberClientConfig;
+import me.frankv.staaaaaaaaaaaack.config.StxckFiberCommonConfig;
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
 
-public class Staaaaaaaaaaaack implements ModInitializer {
-
+public class Staaaaaaaaaaaack implements ModInitializer, ClientModInitializer {
 
     @Override
     public void onInitialize() {
-
+        var config = new StxckFiberCommonConfig();
+        StxckCommon.commonConfig = config;
+        FiberUtils.setup(config.getConfigTree(), StxckFiberCommonConfig.fileName);
     }
+
+    @Override
+    public void onInitializeClient() {
+        var config = new StxckFiberClientConfig();
+        StxckCommon.clientConfig = config;
+        FiberUtils.setup(config.getConfigTree(), StxckFiberClientConfig.fileName);
+    }
+
+
 }
---
+++ Forge/src/main/java/me/frankv/staaaaaaaaaaaack/config/StxckForgeClientConfig.java
@@ -0,0 +1,23 @@
+package me.frankv.staaaaaaaaaaaack.config;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+
+public class StxckForgeClientConfig implements StxckClientConfig {
+    private final ForgeConfigSpec.IntValue minItemCountRenderDistance;
+
+
+    public StxckForgeClientConfig(ForgeConfigSpec.Builder builder) {
+        builder.push("Item count overlay");
+
+        minItemCountRenderDistance = builder
+                .comment("The maximum distance between you and the drops to display its count.")
+                .defineInRange("minItemCountRenderDistance", 8, 0, 128);
+
+        builder.pop();
+    }
+
+    @Override
+    public int getMinItemCountRenderDistance() {
+        return minItemCountRenderDistance.get();
+    }
+}
