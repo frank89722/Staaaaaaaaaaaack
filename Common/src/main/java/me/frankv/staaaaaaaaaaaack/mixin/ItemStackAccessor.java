43131d4146cc2665161f093c19fe17e45cca0dde
frank89722 <mail@frankv.me>
2023-06-04 13:09:50 +0800

    fixes for failed to get acutal maximum stack size on merge items

---
+++ Common/src/main/java/me/frankv/staaaaaaaaaaaack/mixin/ItemStackAccessor.java
@@ -0,0 +1,12 @@
+package me.frankv.staaaaaaaaaaaack.mixin;
+
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(ItemStack.class)
+public interface ItemStackAccessor {
+    @Accessor("item")
+    Item accessItem();
+}
