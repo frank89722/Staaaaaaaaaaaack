4ed11e2b2dfa4ff3997d1a4ca5c9e44fda7380ec
frank89722 <mail@frankv.me>
2023-06-05 21:48:25 +0800

    adds number abbreviation feature for item count overlay

--- Common/src/main/java/me/frankv/staaaaaaaaaaaack/client/ItemCountRenderer.java
+++ Common/src/main/java/me/frankv/staaaaaaaaaaaack/client/ItemCountRenderer.java
@@ -7,7 +7,7 @@ import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.item.ItemEntity;
 
-import static me.frankv.staaaaaaaaaaaack.StxckUtil.getTotalCount;
+import static me.frankv.staaaaaaaaaaaack.StxckUtil.getTotalCountForDisplay;
 
 public class ItemCountRenderer {
     public static void renderItemCount(
@@ -17,23 +17,22 @@ public class ItemCountRenderer {
             int light,
             EntityRenderDispatcherAccessor entityRenderDispatcher
     ) {
-        var itemCount = getTotalCount(entity);
-        if (itemCount <= entity.getItem().getMaxStackSize()) return;
+        getTotalCountForDisplay(entity).ifPresent(itemCount -> {
+            poseStack.pushPose();
+            poseStack.translate(0.0D, entity.getBbHeight() + 0.75f, 0.0D);
+            poseStack.mulPose(entityRenderDispatcher.getCameraOrientation());
+            poseStack.scale(-0.025F, -0.025F, 0.025F);
 
-        poseStack.pushPose();
-        poseStack.translate(0.0D, entity.getBbHeight() + 0.75f, 0.0D);
-        poseStack.mulPose(entityRenderDispatcher.getCameraOrientation());
-        poseStack.scale(-0.025F, -0.025F, 0.025F);
+            var component = Component.nullToEmpty(itemCount);
+            var matrix4f = poseStack.last().pose();
+            var f1 = Minecraft.getInstance().options.getBackgroundOpacity(0.25F);
+            var font = entityRenderDispatcher.getFont();
+            var j = (int)(f1 * 255.0F) << 24;
+            var f2 = (float)(-font.width(component) / 2);
+            font.drawInBatch(component, f2, 0, 553648127, false, matrix4f, bufferSource, false, j, light);
+            font.drawInBatch(component, f2, 0, -1, false, matrix4f, bufferSource, false, 0, light);
 
-        var component = Component.nullToEmpty(String.valueOf(itemCount));
-        var matrix4f = poseStack.last().pose();
-        var f1 = Minecraft.getInstance().options.getBackgroundOpacity(0.25F);
-        var font = entityRenderDispatcher.getFont();
-        var j = (int)(f1 * 255.0F) << 24;
-        var f2 = (float)(-font.width(component) / 2);
-        font.drawInBatch(component, f2, 0, 553648127, false, matrix4f, bufferSource, false, j, light);
-        font.drawInBatch(component, f2, 0, -1, false, matrix4f, bufferSource, false, 0, light);
-
-        poseStack.popPose();
+            poseStack.popPose();
+        });
     }
 }
