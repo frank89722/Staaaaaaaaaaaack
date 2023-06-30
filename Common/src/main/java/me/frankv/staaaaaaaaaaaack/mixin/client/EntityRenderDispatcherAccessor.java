9909ac320e727672033376fb09a5eb9b918c5df6
frank89722 <mail@frankv.me>
2023-06-30 16:08:41 +0800

    cleanup renderer mixin

--- Common/src/main/java/me/frankv/staaaaaaaaaaaack/mixin/client/EntityRenderDispatcherAccessor.java
+++ Common/src/main/java/me/frankv/staaaaaaaaaaaack/mixin/client/EntityRenderDispatcherAccessor.java
@@ -2,11 +2,9 @@ package me.frankv.staaaaaaaaaaaack.mixin.client;
 
 import com.mojang.math.Quaternion;
 import net.minecraft.client.gui.Font;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
-import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(EntityRenderDispatcher.class)
 public interface EntityRenderDispatcherAccessor {
@@ -14,6 +12,4 @@ public interface EntityRenderDispatcherAccessor {
     Font getFont();
     @Accessor("cameraOrientation")
     Quaternion getCameraOrientation();
-    @Invoker("distanceToSqr")
-    double invokeDistanceToSqr(Entity entity);
 }
